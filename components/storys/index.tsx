dd28bfba8fcdbae856db75169dd90293bcf48ef2
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-03-02 19:04:47 -0300

    feat/portifolio: adicionado skeleton e corrigido alinhamento quebra de linha

--- components/storys/index.tsx
+++ components/storys/index.tsx
@@ -17,10 +17,12 @@ interface Story {
       return (
         <div className="max-w-6xl flex flex-col w-[100%] h-auto">
         <h2 className="text-3xl font-bold mb-6">Projetos</h2>
-            <div className="flex flex-row w-[100%]">
-              <div className="flex flex-row m-auto w-20 h-20">
-                <div className="w-12 h-12 border-8 m-auto  border-gray-600 border-t-white rounded-full animate-spin"></div>
-              </div>
+        <div className="flex flex-wrap gap-6 justify-start">
+                <Skeleton/>
+                <Skeleton/>
+                <Skeleton/>
+                <Skeleton/>
+                <Skeleton/>
             </div>
         </div>
       ) 
@@ -30,7 +32,7 @@ interface Story {
       <div className="max-w-6xl mx-auto">
         <h2 className="text-3xl font-bold mb-6">Projetos</h2>
          
-        <div className="flex flex-wrap gap-6 justify-center">
+        <div className="flex flex-wrap gap-6 justify-start">
           {stories.map((story, index) => (
             <div 
               key={index} 
@@ -58,3 +60,34 @@ interface Story {
       </div>
     );
   };
+
+
+
+
+  function Skeleton() {
+    return (
+      <>
+        <div className="text-white select-none bg-[#18181B] border-2 border-gray-800 shadow-md rounded-lg overflow-hidden w-full mdi:w-[48%] md:w-[100%] animate-pulse">
+          <div className="w-full h-48 bg-gray-700"></div>
+  
+          <div className="p-4">
+            <div className="text-xl font-semibold text-transparent bg-gray-600 rounded-lg w-[50%] h-7"></div>
+            <div className="h-3"></div>
+            <div className="text-gray-500 text-sm bg-gray-600 rounded-lg w-[100%] h-5"></div>
+            <div className="h-2"></div>
+            <div className="text-gray-500 text-sm bg-gray-600 rounded-lg w-[100%] h-5"></div>
+            <div className="h-2"></div>
+            <div className="text-gray-500 text-sm bg-gray-600 rounded-lg w-[40%] h-5"></div>
+            <div className="h-2"></div>
+  
+            <div className="flex items-center mt-4">
+              <div className="w-8 h-8 bg-gray-600 rounded-full mr-2"></div>
+              <div className="bg-gray-600 rounded-lg w-[40%] h-5"></div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  
